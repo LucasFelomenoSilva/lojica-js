a6ca3baa875391d157ba621918dc680901818b7c
Lucas <pv.lucassilva@gmail.com>
2024-07-02 07:43:57 -0300

    add 'desafio.js' and update 'app.js'

---
+++ desafio.js
@@ -0,0 +1,17 @@
+alert('Boas vindas ao nosso site!');
+//let nome = 'Lua';
+//let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+let mensagemErro = 'Erro! Preencha todos os campos';
+alert(mensagemErro);
+let nome = prompt('Digite seu nome:')
+let idade = prompt('Digite a sua idade:')
+if (idade >= 18) {
+    alert('Pode tirar habilitação!');
+} else {
+    alert('Não pode tirar habilitação!');
+}
+
+
+
