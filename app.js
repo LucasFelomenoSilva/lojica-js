a0bcf3909cddbca02fd9d10e82c1cff64db8e9a3
Lucas <pv.lucassilva@gmail.com>
2024-07-02 08:41:42 -0300

    add desafio2

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 alert('Boas vindas ao jogo do número secreto');
 
-let chute = prompt('Escolha um número entre 1 e 10');
-console.log('Valor do chute:', chute);
-
 let numeroSecreto = 4;
+let chute;
+console.log('Número secreto: ' + numeroSecreto);
+let tentativas = 1;
 
-console.log('Resultado da comparação:', chute == numeroSecreto);
-
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 10');
 if (chute == numeroSecreto) {
-    alert('Acertou');
+    alert('Isso ai! Você descobriu o número secreto ' + numeroSecreto + ' com ' + tentativas + ' tentativas.');
 } else {
-    console.log('Valor do número secreto:', numeroSecreto);
-    alert('O número secreto era ' + numeroSecreto);
+    if (chute > numeroSecreto) {
+        alert('O número secreto é menor que ' + chute);
+    } else {
+        alert('O número secreto é maior que ' + chute);
+    }
+    tentativas ++;
+ }
 }
